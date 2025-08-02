d688a1d4fbf97655089189a9952b4ac5cc328c2c
Mayur Suresh Gawande <141245128+mayur1gawande@users.noreply.github.com>
2025-08-02 14:09:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ def result():
     return render_template('result.html', prediction_text=prediction_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)),debug=False)
